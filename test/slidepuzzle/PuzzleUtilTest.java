1989692d605bb848ca39cac0f5021bba24522fe4
Jenny Östman <jenny.ostman@yh.nackademin.se>
2018-10-25 11:48:04 +0200

    commit

---
+++ test/slidepuzzle/PuzzleUtilTest.java
@@ -0,0 +1,30 @@
+package slidepuzzle;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class PuzzleUtilTest {
+    
+    PuzzleUtil pu = new PuzzleUtil();
+    
+    @Test
+    public void isNextToEmptyButtonTest(){
+        
+    }
+    
+    @Test
+    public void shuffleButtonsTest(){
+        
+    }
+    
+    @Test
+    public void switchButtonsTest(){
+        
+    }
+    
+    @Test
+    public void isWonTest(){
+        
+    }
+    
+}
